ffc0c3443c05e6889f98f515907b1f6900156074
Mustapha-AJEGHRIR <m5u9s00@gmail.com>
2021-11-08 19:33:06 +0100

    C4 simple, no period_coef

--- two_clustering.py
+++ two_clustering.py
@@ -106,7 +106,7 @@ def online_two_clustering(ring_size, alpha, current_cut, current_cost, new_msg, 
         # # global_state["cut"] = global_state["possible_vals"][index]
         global_state["cut"] = format_me(-1, ring_size)
         # global_state["frec"] = [0]*ring_size
-        global_state["period"] = randint(5,30)*100
+        global_state["period"] = randint(1,40)*100
 
 
     global_state["sigma"].append(new_msg)
@@ -114,7 +114,7 @@ def online_two_clustering(ring_size, alpha, current_cut, current_cost, new_msg, 
     period = global_state["period"]
     if (global_state["call"]) % (period) == 50:
         # costs = np.array(global_state["frec"])
-        frec = build_frec(global_state["sigma"][-period//3:], ring_size)
+        frec = build_frec(global_state["sigma"][-period:], ring_size)
         costs = np.array(frec)
 
         costs = costs + np.concatenate((costs[ring_size//2:], costs[:ring_size//2]))
